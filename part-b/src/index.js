d718480e0c628974e917573a2b45ecfea80e6a05
Shivam Kumar <shivamkumar28499@gmail.com>
2026-07-24 18:19:04 +0530

    sqlite database add for testing

--- part-b/src/index.js
+++ part-b/src/index.js
@@ -1,10 +1,19 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import db from "./models/index.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log("Server on port =", PORT);
-});
+db.sequelize.sync()
+    .then(() => {
+        console.log("Database synced successfully.");
+
+        app.listen(PORT, () => {
+            console.log("Server on port =", PORT);
+        });
+    })
+    .catch((err) => {
+        console.error("Unable to sync database:", err);
+    });
